b31884a38c4de0d218538b353bc445986a1c8d4e
Andy Pan <panjf2000@gmail.com>
2019-09-27 16:28:50 +0800

    Update gnet echo server

--- gnet-echo-server/main.go
+++ gnet-echo-server/main.go
@@ -9,7 +9,6 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"strings"
 
 	"github.com/panjf2000/gnet"
 )
@@ -17,38 +16,25 @@ import (
 func main() {
 	var port int
 	var multicore bool
-	var udp bool
-	var trace bool
-	var reuseport bool
 
 	flag.IntVar(&port, "port", 5000, "server port")
-	flag.BoolVar(&udp, "udp", false, "listen on udp")
-	flag.BoolVar(&reuseport, "reuseport", false, "reuseport (SO_REUSEPORT)")
-	flag.BoolVar(&trace, "trace", false, "print packets to console")
 	flag.BoolVar(&multicore, "multicore", true, "multicore")
 	flag.Parse()
 
 	var events gnet.Events
-	events.Multicore = multicore
 	events.OnInitComplete = func(srv gnet.Server) (action gnet.Action) {
-		log.Printf("echo server started on port %d (loops: %d)", port, srv.NumLoops)
-		if reuseport {
-			log.Printf("reuseport")
-		}
+		log.Printf("Server is running under multi-core: %t, loops: %d\n", srv.Multicore, srv.NumLoops)
 		return
 	}
 	events.React = func(c gnet.Conn) (out []byte, action gnet.Action) {
 		top, tail := c.ReadPair()
-		out = append(top, tail...)
-		c.ResetBuffer()
-		if trace {
-			log.Printf("%s", strings.TrimSpace(string(top)+string(tail)))
+		out = top
+		if tail != nil {
+			out = append(top, tail...)
 		}
+		c.ResetBuffer()
 		return
 	}
 	scheme := "tcp"
-	if udp {
-		scheme = "udp"
-	}
-	log.Fatal(gnet.Serve(events, fmt.Sprintf("%s://:%d", scheme, port)))
+	log.Fatal(gnet.Serve(events, fmt.Sprintf("%s://:%d", scheme, port), gnet.WithMulticore(multicore)))
 }
