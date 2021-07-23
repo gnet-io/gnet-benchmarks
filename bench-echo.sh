e9bf1928597403d52b49e3d60d6f55cbd448268d
Andy Pan <panjf2000@gmail.com>
2021-07-23 17:12:08 +0800

    Update benchmark

--- bench-echo.sh
+++ bench-echo.sh
@@ -21,9 +21,20 @@ $(pkill -9 eviop-echo-server || printf "")
 $(pkill -9 gev-echo-server || printf "")
 $(pkill -9 gnet-echo-server || printf "")
 
+conn_num=$1
+test_duration=$2
+packet_size=$3
+data=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w $packet_size | head -n 1)
+echo "--- ECHO PACKET ---"
+echo $data
+echo ""
+
 function gobench() {
   echo "--- $1 ---"
-  if [[ "$3" != "" ]]; then
+  echo ""
+  if [[ "$1" == "GNET" ]]; then
+    go build -tags=poll_opt -gcflags="-l=4" -ldflags="-s -w" -o $2 $3
+  else
     go build -gcflags="-l=4" -ldflags="-s -w" -o $2 $3
   fi
 
@@ -36,15 +47,16 @@ function gobench() {
   fi
 
   sleep 1
-  echo "*** 300 connections, 30 seconds, 6 byte packets"
+  printf "*** %d connections, %d seconds, packet size: %d bytes\n" $conn_num $test_duration $packet_size
+  
   nl=$'\r\n'
-  tcpkali --workers 8 -c 300 -T 30s -m "PING{$nl}" 127.0.0.1:$4
+  tcpkali -c $conn_num -T $test_duration's' -m $data 127.0.0.1:$4
+  echo ""
   echo "--- DONE ---"
   echo ""
 }
 
 gobench "GO-NET" bin/net-echo-server net-echo-server/main.go 5000
 gobench "EVIO" bin/evio-echo-server evio-echo-server/main.go 5001 -1
-# gobench "EVIOP" bin/eviop-echo-server eviop-echo-server/main.go 5002 -1
 gobench "GEV" bin/gev-echo-server gev-echo-server/echo.go 5003 -1
 gobench "GNET" bin/gnet-echo-server gnet-echo-server/main.go 5004 true
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/panjf2000/gnet_benchmarks
 
-go 1.15
+go 1.16
 
 require (
 	github.com/Allenxuxu/eviop v0.0.0-20190919234625-d421704e9f73
@@ -13,15 +13,12 @@ require (
 	github.com/jung-kurt/gofpdf v1.16.2 // indirect
 	github.com/kavu/go_reuseport v1.5.0 // indirect
 	github.com/klauspost/compress v1.10.10 // indirect
-	github.com/panjf2000/gnet v1.4.3
+	github.com/panjf2000/gnet v1.5.3
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/tidwall/evio v1.0.7
 	github.com/valyala/fasthttp v1.14.0
 	golang.org/x/exp v0.0.0-20191129062945-2f5052295587 // indirect
 	golang.org/x/image v0.0.0-20200618115811-c13761719519 // indirect
-	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c // indirect
 	gonum.org/v1/gonum v0.6.1 // indirect
-	gonum.org/v1/netlib v0.0.0-20191031114514-eccb95939662 // indirect
 	gonum.org/v1/plot v0.7.0
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
